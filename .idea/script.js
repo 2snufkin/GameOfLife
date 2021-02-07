fdc7719a9dcf6de746bf6aaa9d850f3a16829974
2snufkin <75860271+2snufkin@users.noreply.github.com>
2021-02-07 19:55:21 +0100

    mid

--- .idea/script.js
+++ .idea/script.js
@@ -1,9 +1,18 @@
-'use strict1'
+//NoNigbrs1 = Number od living neigbourous (neigbourous  ===1 )
+//if you are 0
+//NoNigbrs1 === 3? 1 : 0
+
+//if you are 1
+//NoNigbrs1 === 3|| NoNigbrs === 2|? 1 : 0
+
+'use strict'
 let rows;
 let col;
 let grid;
 let reso = 20;
 
+
+//-------------------------------------My Functions--------------------------------------------------
 function make2DArray(col,rows){
     let arr = new Array(col);
     for (let i=0; i<col; i++){
@@ -13,6 +22,11 @@ function make2DArray(col,rows){
 }
 
 
+
+
+
+
+//***********************************Built in Functions-------------------------------------------
 function setup(){
     createCanvas(600, 400);
     col = width /reso;
@@ -21,9 +35,11 @@ function setup(){
     line(0, 0, width, height);
     background(0);
     grid= make2DArray(col,rows);
+    gridNewGen= make2DArray(col,rows);
+
     for (let i=0; i<col ;i++){
         for(let j=0; j<rows; j++){
-            grid[i][j] = Math.floor(random(2));
+            grid[i][j] =  floor(random(2));
 
         }
 
@@ -34,7 +50,7 @@ function setup(){
 function draw(){
     for (let i=0; i<col ;i++) {
         for (let j = 0; j < rows; j++) {
-           
+
             let x = i* reso;
             let y = j * reso;
             if (grid[i][j] === 0) {
